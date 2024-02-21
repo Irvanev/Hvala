2dcef5b81c1cf1ba721f06aba2a5032e9978a04d
irvanev <irvvanevv@mail.ru>
2024-02-21 10:53:04 +0300

    Added start page for chat

--- src/pages/message/Message.js
+++ src/pages/message/Message.js
@@ -1,50 +1,104 @@
-import ListGroup from 'react-bootstrap/ListGroup';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Tab from 'react-bootstrap/Tab';
-import {Container} from "react-bootstrap";
+import {db} from "../../config/firebase";
+import {Badge, Container, ListGroup, Image} from 'react-bootstrap';
+import { auth } from "../../config/firebase";
 import {MyNavbar} from '../../components/Navbar/Navbar';
-
+import React from "react";
+import {useEffect, useState} from 'react';
+import {collection, getDocs, query, where} from 'firebase/firestore';
+import {useHistory} from "react-router-dom";
+import Logo from'../../assets/logo.png';
 export const Message = () => {
+    const history = useHistory();
+    const [messages, setMessages] = useState([]);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const loadMessages = async () => {
+            const currentUser = auth.currentUser;
+            const currentUserId = currentUser ? currentUser.uid : null;
+
+            const messagesCollection = collection(db, 'message');
+            const messagesQuery = query(messagesCollection, where('from_uid', '==', currentUserId));
+            const messagesSnapshot = await getDocs(messagesQuery);
+            setMessages(messagesSnapshot.docs.map(doc => doc.data()));
+        };
+
+        const loadUsers = async () => {
+            const usersCollection = collection(db, 'users');
+            const usersSnapshot = await getDocs(usersCollection);
+            setUsers(usersSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+        };
+
+        loadMessages();
+        loadUsers();
+    }, []);
+
+    const goBack = () => {
+        history.goBack();
+    }
+
     return (
         <div>
-            <MyNavbar />
-            <Container style={{paddingTop: '20px'}}>
-                <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
-                    <Row>
-                        <Col sm={4}>
-                            <ListGroup>
-                                <ListGroup.Item action href="#link1">
-                                    Link 1
-                                </ListGroup.Item>
-                                <ListGroup.Item action href="#link2">
-                                    Link 2
-                                </ListGroup.Item>
-                                <ListGroup.Item action href="#link3">
-                                    Link 2
-                                </ListGroup.Item>
-                                <ListGroup.Item action href="#link4">
-                                    Link 2
-                                </ListGroup.Item>
-                                <ListGroup.Item action href="#link5">
-                                    Link 2
-                                </ListGroup.Item>
-                                <ListGroup.Item action href="#link6">
-                                    Link 2
-                                </ListGroup.Item>
-                                <ListGroup.Item action href="#link7">
-                                    Link 2
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Col>
-                        <Col sm={8}>
-                            <Tab.Content>
-                                <Tab.Pane eventKey="#link1">Tab pane content 1</Tab.Pane>
-                                <Tab.Pane eventKey="#link2">Tab pane content 2</Tab.Pane>
-                            </Tab.Content>
-                        </Col>
-                    </Row>
-                </Tab.Container>
+
+            <style type="text/css">
+                {`
+                @media (max-width: 1000px) {
+                    body {
+                        padding-bottom: 6.0rem;
+                        padding-top: 4.5rem;
+                    }
+                }
+                @media (min-width: 1000px) {
+                    body {
+                        padding-top: 4.5rem;
+                    }
+                }
+                `}
+            </style>
+
+            <MyNavbar/>
+
+            <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light d-lg-none">
+                <div className="container">
+                    <ul className="navbar-nav me-auto mb-md-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" onClick={goBack}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                                     className="bi bi-arrow-left" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd"
+                                          d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <Container>
+                <ListGroup as="ul">
+                    {messages.map((message, index) => {
+                        const user = users.find(user => user.id === message.to_uid);
+
+                        return (
+                            <ListGroup.Item
+                                key={index}
+                                as="li"
+                                className="d-flex justify-content-between align-items-start"
+                            >
+                                <div className="d-flex align-items-center">
+                                    {user && <Image src={user.photoUrl || Logo} roundedCircle width="50" height="50" className="me-2" />}
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">{message.to_name}</div>
+                                        {message.msglist ? message.msglist.join(', ') : ''}
+                                    </div>
+                                </div>
+                                <Badge bg="primary" pill>
+                                    {message.msglist ? message.msglist.length : 0}
+                                </Badge>
+                            </ListGroup.Item>
+                        );
+                    })}
+                </ListGroup>
             </Container>
         </div>
     )
