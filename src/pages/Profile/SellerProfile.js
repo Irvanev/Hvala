d2ecae6e12fa45281897105c2ab268b537f4e718
irvanev <irvvanevv@mail.ru>
2024-02-23 12:14:34 +0300

    Added sellerProfile for mobile version

--- src/pages/Profile/SellerProfile.js
+++ src/pages/Profile/SellerProfile.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Image, Card } from "react-bootstrap";
 import Logo from "../../assets/logo.png";
 import { MyNavbar } from "../../components/Navbar/Navbar";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { Link } from "react-router-dom";
@@ -11,6 +11,7 @@ const SellerProfile = () => {
   const { id } = useParams();
   const [user, setUser] = useState(null);
   const [ads, setAds] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     const fetchUserAndAds = async () => {
@@ -40,6 +41,9 @@ const SellerProfile = () => {
   const aStyle = {
     textDecoration: "none",
   };
+  const goBack = () => {
+    history.goBack();
+  };
 
   return (
     <div>
@@ -59,7 +63,7 @@ const SellerProfile = () => {
                   }
                   @media (max-width: 1000px) {
                       body {
-                          padding-bottom: 3.5rem;
+                          padding-bottom: 5.5rem;
                           padding-top: 3.5rem;
                       }
                       .imageAdvertisment {
@@ -73,6 +77,7 @@ const SellerProfile = () => {
                   }
                   @media (min-width: 1000px) {
                     body {
+                      padding-bottom: 3.5rem;
                           padding-top: 4.5rem;
                       }
                       .imageAdvertisment {
@@ -140,6 +145,112 @@ const SellerProfile = () => {
           <Col xs={9}>
             <Container className="album mt-3">
               <Row xs={2} sm={2} md={3} lg={4} className="g-3" id="cardAds">
+                {user &&
+                  ads &&
+                  ads.map((advertisment, index) => (
+                    <Col key={index}>
+                      <Link
+                        key={advertisment.id}
+                        to={`/advertisment/${advertisment.id}`}
+                        style={aStyle}
+                      >
+                        <Card className="shadow-sm">
+                          <Card.Img
+                            variant="top"
+                            src={
+                              (advertisment.photoUrls &&
+                                advertisment.photoUrls[0]) ||
+                              Logo
+                            }
+                            alt="imageAdvertisment"
+                            className="imageAdvertisment"
+                          />
+                          <Card.Body>
+                            <Card.Text>
+                              <span className="location-text">
+                                {advertisment.title}
+                              </span>
+                              <strong>
+                                {advertisment.price + "€"}
+                                <br />
+                              </strong>
+                              <span className="location-text">
+                                {advertisment.location}
+                              </span>
+                              <span className="date-text">
+                                {new Date(
+                                  advertisment.time_creation.seconds * 1000
+                                ).toLocaleString("ru", {
+                                  day: "numeric",
+                                  month: "long",
+                                  hour: "2-digit",
+                                  minute: "2-digit",
+                                })}
+                              </span>
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Link>
+                    </Col>
+                  ))}
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+
+      <nav className="navbar navbar-expand-md navbar-light fixed-top bg-light d-lg-none">
+        <div className="container">
+          <ul className="navbar-nav me-auto mb-md-0">
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                onClick={goBack}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="currentColor"
+                  className="bi bi-arrow-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                  />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Container className="d-lg-none">
+        <Row className="text-center">
+          <Col>
+            <div className="profile-picture my-3">
+              <Image src={user?.photoURL || Logo} alt="photoProfile" id="userPhoto" className="mx-auto" />
+            </div>
+            <h2 className="profile-name" id="userName">{user?.name}</h2>
+            {user && (
+              <div className="profile-reviews">
+                <p id="kolRating">
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <span key={index}>{index < user.raiting ? "⭐" : "☆"}</span>
+                  ))}
+                </p>
+              </div>
+            )}
+            <div className="profile-sections">
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Container className="album mt-3">
+              <Row xs={2} sm={2} className="g-3" id="cardAds">
                 {user &&
                   ads &&
                   ads.map((advertisment, index) => (
