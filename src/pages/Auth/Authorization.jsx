aa23ab47078d08493d25f9c74017751dc1a634fb
irvanev <irvvanevv@mail.ru>
2024-04-17 13:01:06 +0300

    Added validation and redisign

--- src/pages/Auth/Authorization.jsx
+++ src/pages/Auth/Authorization.jsx
@@ -1,25 +1,37 @@
-import {useState} from 'react';
-import {useHistory} from 'react-router-dom';
-import {signInWithEmailAndPassword} from 'firebase/auth';
-import { Container, Form, Button } from 'react-bootstrap';
-import {useTranslation} from 'react-i18next';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { signInWithEmailAndPassword, setPersistence, browserSessionPersistence, browserLocalPersistence } from 'firebase/auth';
+import { Form, Input, Checkbox, Button, Typography } from 'antd';
+import { useTranslation } from 'react-i18next';
 import Logotype from "../../assets/logo.png"
-import {auth} from "../../config/firebase"
-import {MyNavbar} from '../../components/Navbar/Navbar';
+import { auth } from "../../config/firebase"
+import { MyNavbar } from '../../components/Navbar/Navbar';
 
 export const Authorization = () => {
-    const {t} = useTranslation();
+    const { Link } = Typography;
+    const { t } = useTranslation();
+    const history = useHistory();
+
+    const [rememberMe, setRememberMe] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const history = useHistory();
+    const [loginError, setLoginError] = useState('');
+
+    const onRememberMeChange = (e) => {
+        setRememberMe(e.target.checked);
+    };
+
 
     const goBack = () => {
         history.goBack();
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const handleSubmit = async (values) => {
+        const { email, password } = values;
         try {
+            const persistence = rememberMe ? browserLocalPersistence : browserSessionPersistence;
+            await setPersistence(auth, persistence);
+    
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
             const user = userCredential.user;
     
@@ -28,12 +40,16 @@ export const Authorization = () => {
                 localStorage.setItem('userId', userId);
                 history.push('/advertisment');
             } else {
-                alert('Пожалуйста, подтвердите свой адрес электронной почты перед входом.');
+                setLoginError('Пожалуйста, подтвердите свой адрес электронной почты перед входом.');
             }
         } catch (error) {
-            alert(error.message);
+            if (error.code === 'auth/user-not-found' || error.code === 'auth/wrong-password') {
+                setLoginError('Неверный логин или пароль.');
+            } else {
+                setLoginError(error.message);
+            }
         }
-    }
+    };
 
     return (
         <div>
@@ -61,9 +77,9 @@ export const Authorization = () => {
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" onClick={goBack}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
-                                     className="bi bi-arrow-left" viewBox="0 0 16 16">
+                                    className="bi bi-arrow-left" viewBox="0 0 16 16">
                                     <path fillRule="evenodd"
-                                          d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                                 </svg>
                             </a>
                         </li>
@@ -71,34 +87,49 @@ export const Authorization = () => {
                 </div>
             </nav>
 
-            <MyNavbar/>
+            <MyNavbar />
 
-            <div style={{paddingTop: '4.5rem'}}>
-                <img src={Logotype} alt="logo" style={{width: "200px", height: "200px", borderRadius: "50%", display: "block", margin: "auto"}}/>
+            <div className='container'>
+                <div style={{ paddingTop: '1.5rem' }}>
+                    <img src={Logotype} alt="logo" style={{ width: "200px", height: "200px", borderRadius: "50%", display: "block", margin: "auto" }} />
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: "center" }}>
+                    <Form layout="vertical" onFinish={handleSubmit} style={{ width: "400px" }} autoComplete="on">
+                        <Form.Item
+                            label={t('email')}
+                            name='email'
+                            rules={[
+                                { required: true, message: 'Пожалуйста, введите ваш email!' },
+                                { type: 'email', message: 'Пожалуйста, введите корректный email!' }
+                            ]}
+                        >
+                            <Input type="email" id="email" name='email' placeholder="name@example.com" value={email}
+                                onChange={e => setEmail(e.target.value)} />
+                        </Form.Item>
+                        <Form.Item
+                            label={t('password')}
+                            name='password'
+                            rules={[
+                                { required: true, message: 'Пожалуйста, введите ваш пароль!' },
+                                { min: 6, message: 'Пароль должен содержать минимум 6 символов!' }
+                            ]}
+                        >
+                            <Input.Password id="password" name='password' value={password}
+                                onChange={e => setPassword(e.target.value)} />
+                        </Form.Item>
+                        {loginError && <p style={{ color: 'red' }}>{loginError}</p>}
+                        <Form.Item name="saveSession" valuePropName="checked">
+                            <Checkbox onChange={onRememberMeChange}>{t('remember_me')}</Checkbox>
+                        </Form.Item>
+                        <Form.Item>
+                            <p>{t('notRegisteredYet')} <Link href="/sign_up">{t('register')}</Link></p>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button size='large' type="primary" htmlType="submit" id="login">{t('login')}</Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
-            <Container style={{ display: 'flex', alignItems: 'center', justifyContent: "center"}}>
-                <Form onSubmit={handleSubmit} style={{width: "400px"}} autocomplete="on">
-                    <Form.Group className="mb-3">
-                        <Form.Label>{t('email')}</Form.Label>
-                        <Form.Control type="email" id="email" name='email' placeholder="name@example.com" value={email}
-                                      onChange={e => setEmail(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>{t('password')}</Form.Label>
-                        <Form.Control type="password" id="password" name='password' value={password}
-                                      onChange={e => setPassword(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="saveSession">
-                        <Form.Check type="checkbox" label={t('remember_me')}/>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <p>{t('notRegisteredYet')} <a href="/sign_up">{t('register')}</a></p>
-                    </Form.Group>
-                    <div>
-                        <Button type="submit" className="btn" id="login">{t('login')}</Button>
-                    </div>
-                </Form>
-            </Container>
         </div>
     );
 }
--- src/pages/Register/Registration.jsx
+++ src/pages/Register/Registration.jsx
@@ -5,20 +5,46 @@ import { createUserWithEmailAndPassword, sendEmailVerification, onAuthStateChang
 import { doc, setDoc, serverTimestamp, collection, query, where, getDocs } from "firebase/firestore";
 import { useHistory } from 'react-router-dom';
 import { MyNavbar } from '../../components/Navbar/Navbar';
-import { Button, Container, Form } from "react-bootstrap";
-import {useTranslation} from 'react-i18next';
+import { Form, Input, Button, Modal } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 export const Registration = () => {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
     const [confPassword, setConfPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [loginError, setLoginError] = useState('');
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = async () => {
+        setIsModalVisible(false);
+        history.push('/sign_in');
+            await signOut(auth);
+            localStorage.clear();
+    };
+
+    const handleCancel = async () => {
+        setIsModalVisible(false);
+        history.push('/sign_in');
+            await signOut(auth);
+            localStorage.clear();
+    };
 
     const checkUsername = async (username) => {
         const q = query(collection(db, "users"), where("name", "==", username));
+        const querySnapshot = await getDocs(q);
+        return !querySnapshot.empty;
+    }
+
+    const checkEmail = async (email) => {
+        const q = query(collection(db, "users"), where("email", "==", email));
         const querySnapshot = await getDocs(q);
         return !querySnapshot.empty;
     }
@@ -28,14 +54,12 @@ export const Registration = () => {
     }
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
         if (password !== confPassword) {
-            setErrorMessage(t('passwordsDoNotMatch'));
             return;
         }
         const isUsernameTaken = await checkUsername(username);
         if (isUsernameTaken) {
-            alert(t('usernameTaken'));
+            setLoginError('Имя пользвателя уже занято!');
             return;
         }
         try {
@@ -43,7 +67,7 @@ export const Registration = () => {
             const user = userCredential.user;
 
             await sendEmailVerification(user);
-            alert(t('confirmEmail'));
+            showModal();
 
             await setDoc(doc(db, 'users', user.uid), {
                 addtime: serverTimestamp(),
@@ -51,14 +75,11 @@ export const Registration = () => {
                 fcmtoken: "",
                 location: "",
                 photoUrl: "",
-                raiting: 0,
+                rating: 0,
                 name: username,
                 email: email,
                 id: user.uid
             });
-            history.push('/sign_in');
-            await signOut(auth);
-            localStorage.clear();
         } catch (error) {
             alert(error.message);
         }
@@ -102,41 +123,95 @@ export const Registration = () => {
 
             <MyNavbar />
 
-            <div style={{ paddingTop: '4.5rem' }}>
-                <img src={Logotype} alt="logo"
-                    style={{ width: "200px", height: "200px", borderRadius: "50%", display: "block", margin: "auto" }} />
+            <div className='container'>
+                <div style={{ paddingTop: '1.5rem' }}>
+                    <img src={Logotype} alt="logo"
+                        style={{ width: "200px", height: "200px", borderRadius: "50%", display: "block", margin: "auto" }} />
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: "center" }}>
+                    <Form layout='vertical' onFinish={handleSubmit} style={{ width: "400px" }}>
+                        <Form.Item
+                            label={t('email')}
+                            name="email"
+                            rules={[
+                                { required: true, message: 'Пожалуйста, введите ваш email!' },
+                                { type: 'email', message: 'Пожалуйста, введите корректный email!' },
+                                () => ({
+                                    validator(_, value) {
+                                        return checkEmail(value).then(isEmailTaken => {
+                                            if (isEmailTaken) {
+                                                return Promise.reject('Email уже занят');
+                                            }
+                                            return Promise.resolve();
+                                        });
+                                    },
+                                }),
+                            ]}
+                        >
+                            <Input type="email" placeholder="name@example.com" value={email}
+                                onChange={e => setEmail(e.target.value)} />
+                        </Form.Item>
+                        <Form.Item
+                            label={t('username')}
+                            name="username"
+                            rules={[
+                                { required: true, message: 'Пожалуйста, введите ваше имя пользователя!' },
+                                () => ({
+                                    validator(_, value) {
+                                        return checkUsername(value).then(isUsernameTaken => {
+                                            if (isUsernameTaken) {
+                                                return Promise.reject('Имя пользователя уже занято');
+                                            }
+                                            return Promise.resolve();
+                                        });
+                                    },
+                                }),
+                            ]}
+                        >
+                            <Input type="text" value={username} autoComplete="username"
+                                onChange={e => setUsername(e.target.value)} />
+                        </Form.Item>
+                        <Form.Item
+                            label={t('password')}
+                            name="password"
+                            rules={[
+                                { required: true, message: 'Пожалуйста, введите ваш пароль!' },
+                                { min: 6, message: 'Пароль должен содержать минимум 6 символов!' }
+                            ]}
+                        >
+                            <Input.Password value={password} autoComplete="new-password"
+                                onChange={e => setPassword(e.target.value)} />
+                        </Form.Item>
+                        <Form.Item
+                            label={t('confirmPassword')}
+                            name="confirmPassword"
+                            rules={[
+                                { required: true, message: 'Пожалуйста, подтвердите ваш пароль!' },
+                                ({ getFieldValue }) => ({
+                                    validator(_, value) {
+                                        if (!value || getFieldValue('password') === value) {
+                                            return Promise.resolve();
+                                        }
+                                        return Promise.reject(new Error('Пароли не совпадают!'));
+                                    },
+                                }),
+                            ]}
+                        >
+                            <Input.Password value={confPassword} autoComplete="new-password"
+                                onChange={e => setConfPassword(e.target.value)} />
+                        </Form.Item>
+                        <Form.Item>
+                            <p>{t('alreadyRegistered')} <a href="/sign_in">{t('login')}</a></p>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button size='large' type="primary" htmlType="submit" id="login">{t('register')}</Button>
+                        </Form.Item>
+                        <Modal title="Подтверждение Email" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                            <p>{t('confirmEmail')}</p>
+                        </Modal>
+                    </Form>
+                </div>
             </div>
-            <Container style={{ display: 'flex', alignItems: 'center', justifyContent: "center" }}>
-                <Form onSubmit={handleSubmit} style={{ width: "400px" }}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>{t('email')}</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" value={email}
-                            onChange={e => setEmail(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>{t('username')}</Form.Label>
-                        <Form.Control type="text" value={username} autoComplete="username"
-                            onChange={e => setUsername(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>{t('password')}</Form.Label>
-                        <Form.Control type="password" value={password} autoComplete="new-password"
-                            onChange={e => setPassword(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>{t('confirmPassword')}</Form.Label>
-                        <Form.Control type="password" value={confPassword} autoComplete="new-password"
-                            onChange={e => setConfPassword(e.target.value)} />
-                        <span style={{ color: 'red' }}>{errorMessage}</span>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <p>{t('alreadyRegistered')} <a href="/sign_in">{t('login')}</a></p>
-                    </Form.Group>
-                    <div>
-                        <Button type="submit" className="btn" id="login">{t('register')}</Button>
-                    </div>
-                </Form>
-            </Container>
         </div>
     );
 }
