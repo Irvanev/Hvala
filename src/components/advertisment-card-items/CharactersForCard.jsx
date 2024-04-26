2df9a8407bfde390995e0405f2e76ee90083bf28
Salian17 <sazonov1744@yandex.ru>
2024-04-26 18:54:03 +0300

    add characters

--- src/components/advertisment-card-items/CharactersForCard.jsx
+++ src/components/advertisment-card-items/CharactersForCard.jsx
@@ -60,6 +60,104 @@ const CharactersForCard = ({ adData, t }) => {
                         <span id="product-description">{t(adData.location)}</span>
                     </div>
                 )}
+                {adData?.title && (
+                    <div>
+                        <span id="product-description">
+                            {t('title')}: <strong>{t(adData.title)}</strong> 
+                        </span>
+                    </div>
+                )}
+                {adData?.price && (
+                    <div>
+                        <span id="product-description">
+                            {t('price')}: <strong>{t(adData.price)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.region && (
+                    <div>
+                        <span id="production-description">
+                            {t('region')}: <strong>{t(adData.region)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.country && (
+                    <div>
+                        <span id="production-description">
+                            {t('country')}: <strong>{t(adData.country)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.type && (
+                    <div>
+                        <span id="production-description">
+                            {t('type')}:<strong>{t(adData.type)}</strong> 
+                        </span>
+                    </div>
+                )}
+                {adData?.miliage &&(
+                    <div>
+                        <span id="production-description">
+                            {t('milliage')}: <strong>{t(adData.miliage)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.year &&(
+                    <div>
+                        <span id="production-description">
+                            {t('year')}: <strong>{t(adData.year)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData.body &&(
+                    <div>
+                        <span id="production-description">
+                            {t('body')}: <strong>{t(adData.body)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.сolor &&(
+                    <div>
+                        <span id="production-description">
+                            {t('color')}: <strong>{t(adData.сolor)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.transsmission &&(
+                    <div>
+                        <span id="production-description">
+                            {t('transmission')}: <strong>{t(adData.transsmission)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.drive &&(
+                    <div>
+                        <span id="production-description">
+                            {t('drive')}: <strong>{t(adData.drive)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.wheel &&(
+                    <div>
+                        <span id="production-description">
+                            {t('wheel')}: <strong>{t(adData.wheel)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.owners &&(
+                    <div>
+                        <span id="production-description">
+                            {t('owners')}: <strong>{t(adData.owners)}</strong>
+                        </span>
+                    </div>
+                )}
+                {adData?.customs &&(
+                    <div>
+                        <span id="production-description">
+                            {t('customs')}: <strong>{t(adData.customs)}</strong>
+                        </span>
+                    </div>
+                )}
                 {adData?.coordinates && (
                     <div className="mt-3">
                         <iframe
