f8b02b5d88adc1533c3f78f9c9dc6773a6e8f500
irvanev <irvvanevv@mail.ru>
2024-08-16 16:53:13 +0300

    Fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -152,41 +152,41 @@ export const MyNavbar = () => {
 
                 <div class="fixed bottom-0 left-0 z-50 w-full h-16 bg-customColor1 border-t rounded-t-2xl border-gray-200">
                     <div class="grid h-full max-w-lg grid-cols-5 mx-auto font-medium">
-                        <button onClick={handleHomeClick} type="button" className="inline-flex flex-col items-center justify-center px-5 group е">
+                        <button onClick={handleHomeClick} type="button" className="inline-flex flex-col items-center justify-center px-4 group е">
                             <svg class={`w-6 h-6 ${getButtonStyle('/advertisment')}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd" />
                             </svg>
-                            <span class={`text-sm text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/advertisment')}`}>{t("home_navbar")}</span>
+                            <span class={`text-xs text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/advertisment')}`}>{t("home_navbar")}</span>
                         </button>
-                        <button onClick={handleSettingsClick} type="button" class="inline-flex flex-col items-center justify-center px-5 border-gray-200 group dark:border-gray-600">
+                        <button onClick={handleSettingsClick} type="button" class="inline-flex flex-col items-center justify-center px-4 border-gray-200 group dark:border-gray-600">
                             <svg class={`w-6 h-6 ${getButtonStyle('/settings')}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path d="M5 13.17a3.001 3.001 0 0 0 0 5.66V20a1 1 0 1 0 2 0v-1.17a3.001 3.001 0 0 0 0-5.66V4a1 1 0 0 0-2 0v9.17ZM11 20v-9.17a3.001 3.001 0 0 1 0-5.66V4a1 1 0 1 1 2 0v1.17a3.001 3.001 0 0 1 0 5.66V20a1 1 0 1 1-2 0Zm6-1.17V20a1 1 0 1 0 2 0v-1.17a3.001 3.001 0 0 0 0-5.66V4a1 1 0 1 0-2 0v9.17a3.001 3.001 0 0 0 0 5.66Z" />
                             </svg>
-                            <span class={`text-sm text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/settings')}`}>{t("settings")}</span>
+                            <span class={`text-xs text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/settings')}`}>{t("settings")}</span>
                         </button>
 
 
-                        <button onClick={handleAddItemClick} type="button" class="inline-flex flex-col items-center justify-center px-5 group">
+                        <button onClick={handleAddItemClick} type="button" class="inline-flex flex-col items-center justify-center px-4 group">
                             <svg class={`w-6 h-6 ${getButtonStyle('/addItem')}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
                             </svg>
-                            <span class={`text-sm text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/addItem')}`}>{t("addItem_navbar")}</span>
+                            <span class={`text-xs text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/addItem')}`}>{t("addItem_navbar")}</span>
                         </button>
 
 
-                        <button onClick={handleMessageClick} type="button" class="inline-flex flex-col items-center justify-center px-5 group">
+                        <button onClick={handleMessageClick} type="button" class="inline-flex flex-col items-center justify-center px-4 group">
                             <svg class={`w-6 h-6 ${getButtonStyle('/message')}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M4 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h1v2a1 1 0 0 0 1.707.707L9.414 13H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4Z" clip-rule="evenodd" />
                                 <path fill-rule="evenodd" d="M8.023 17.215c.033-.03.066-.062.098-.094L10.243 15H15a3 3 0 0 0 3-3V8h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1v2a1 1 0 0 1-1.707.707L14.586 18H9a1 1 0 0 1-.977-.785Z" clip-rule="evenodd" />
                             </svg>
-                            <span class={`text-sm text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/message')}`}>{t("message_navbar")}</span>
+                            <span class={`text-xs text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/message')}`}>{t("message_navbar")}</span>
                         </button>
 
-                        <button onClick={handleProfileClick} type="button" class="inline-flex flex-col items-center justify-center px-5 border-gray-200 group dark:border-gray-600">
+                        <button onClick={handleProfileClick} type="button" class="inline-flex flex-col items-center justify-center px-4 border-gray-200 group dark:border-gray-600">
                             <svg class={`w-6 h-6 ${getButtonStyle('/profile')}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd" />
                             </svg>
-                            <span class={`text-sm text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/profile')}`}>{t("profile_navbar")}</span>
+                            <span class={`text-xs text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/profile')}`}>{t("profile_navbar")}</span>
                         </button>
                     </div>
                 </div>
