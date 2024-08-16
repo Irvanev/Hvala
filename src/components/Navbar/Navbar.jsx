80bccac0fe5d11ae197a682046fa3a3112feea5c
irvanev <irvvanevv@mail.ru>
2024-08-16 16:44:32 +0300

    Fix button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,6 +47,7 @@ export const MyNavbar = () => {
     };
     const handleAddItemClick = () => {
         history.push('/addItem');
+        window.location.reload();
     };
     const handleMessageClick = () => {
         history.push('/message');
@@ -166,12 +167,10 @@ export const MyNavbar = () => {
 
 
                         <button onClick={handleAddItemClick} type="button" class="inline-flex flex-col items-center justify-center px-5 group">
-                            <a href=''>
-                                <svg class={`w-6 h-6 ${getButtonStyle('/addItem')}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
-                                </svg>
-                                <span class={`text-sm text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/addItem')}`}>{t("addItem_navbar")}</span>
-                            </a>
+                            <svg class={`w-6 h-6 ${getButtonStyle('/addItem')}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
+                            </svg>
+                            <span class={`text-sm text-customColor3  group-hover:text-customColor2 ${getButtonStyle('/addItem')}`}>{t("addItem_navbar")}</span>
                         </button>
 
 
